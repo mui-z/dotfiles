cff084d399eaa0abe18572a612a5736453250f03
mui-z <93278577+mui-z@users.noreply.github.com>
2023-05-09 00:01:29 +0900

    first commit

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require 'wezterm';
+
+return {
+  use_ime = true,
+  font_size = 11.0,
+  color_scheme = "iceberg-dark", 
+  hide_tab_bar_if_only_one_tab = true,
+  adjust_window_size_when_changing_font_size = false,
+  window_background_image = os.getenv('HOME') .. '/Downloads/background.jpeg',
+  window_background_image_hsb = {
+      brightness = 0.02,
+      hue = 1.0,
+      saturation = 1.0
+  }
+}
+
